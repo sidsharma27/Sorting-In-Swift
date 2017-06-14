6f197ddb91205f204f4d5392680a94a45670a9d0
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-06-14 13:05:12 -0700

    Fixed Test name

--- sorting/bubbleSort/Tests/bubbleSortTests/bubbleSortTests.swift
+++ sorting/bubbleSort/Tests/bubbleSortTests/bubbleSortTests.swift
@@ -5,12 +5,12 @@ class bubbleSortTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(bubbleSort().bubbleSort(array:[3,1,2]), [1, 2, 3])
-        XCTAssertEqual(bubbleSort().bubbleSort(array: [18,22,23,20,-20,2,3,14]), [-20, 2, 3, 14, 18, 20, 22, 23])
+        XCTAssertEqual(testing().bubbleSort(array:[3,1,2]), [1, 2, 3])
+        XCTAssertEqual(testing().bubbleSort(array: [18,22,23,20,-20,2,3,14]), [-20, 2, 3, 14, 18, 20, 22, 23])
     }
 
 
-    public static var test = [
+    static var allTests = [
         ("testExample", testExample),
     ]
 }
